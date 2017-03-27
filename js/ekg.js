317ca821a1431934fc75c3f896467e2ff71f26a8
jean-philippe legacy <jean-philippe legacy>
2017-03-27 15:34:15 -0400

    updated js file for animation, it glitches so were using css3

--- js/ekg.js
+++ js/ekg.js
@@ -7,18 +7,23 @@
 	console.log("SEAF Fired");
 	//variables
 
-	var ekg = document.querySelector('#line');
+	var ekg = document.querySelector('#ekg');
 	var flatline = document.querySelector('#flatline');
+	var heart = document.querySelector('#heart');
 
 	//functions
+
 
 	function ekgPump(){
 		console.log("ekgPump");
 
 		TweenLite.to("#flatline",0.1,{css:{opacity:"0"}});
-		TweenLite.fromTo(ekg,1,{css:{opacity:"0.0000001"}},{css:{opacity:"1"}});
-		TweenLite.fromTo(ekg,0.5,{drawSVG:"0%"},{drawSVG:"30% 100%"});
-		TweenLite.fromTo(ekg,1,{css:{opacity:"1"}},{css:{opacity:"0",delay:1}});
+		TweenLite.fromTo(ekg,0.7,{css:{opacity:"0.0000001"}},{css:{opacity:"1"}});
+		TweenLite.fromTo(ekg,0.5,{drawSVG:"0%"},{drawSVG:"0% 100%"});
+		TweenLite.fromTo(ekg,0.5,{drawSVG:"100%"},{drawSVG:"98% 100%",delay:1.85});
+		TweenLite.fromTo(ekg,0.7,{css:{opacity:"1"}},{css:{opacity:"0"},delay:1.85});
+
+		TweenLite.fromTo(heart,0.3,{scale:1},{scale:0.99});
 
 		setTimeout(ekgPump2,2000);
 	}
@@ -26,9 +31,10 @@
 	function ekgPump2(){
 		console.log("ekgPump2");
 
-		TweenLite.fromTo(ekg,1,{css:{opacity:"0.0000001"}},{css:{opacity:"1"}});
-		TweenLite.fromTo(ekg,0.5,{drawSVG:"0%"},{drawSVG:"30% 100%"});
-		TweenLite.fromTo(ekg,1,{css:{opacity:"1"}},{css:{opacity:"0",delay:1}});
+		TweenLite.fromTo(ekg,0.7,{css:{opacity:"0.0000001"}},{css:{opacity:"1"}});
+		TweenLite.fromTo(ekg,0.5,{drawSVG:"0%"},{drawSVG:"0% 100%"});
+		TweenLite.fromTo(ekg,0.5,{drawSVG:"100%"},{drawSVG:"98% 100%",delay:1.85});
+		TweenLite.fromTo(ekg,0.7,{css:{opacity:"1"}},{css:{opacity:"0"},delay:1.85});
 
 		setTimeout(ekgPump3,2000);
 	}
@@ -36,9 +42,10 @@
 	function ekgPump3(){
 		console.log("ekgPump3");
 
-		TweenLite.fromTo(ekg,1,{css:{opacity:"0.0000001"}},{css:{opacity:"1"}});
-		TweenLite.fromTo(ekg,0.5,{drawSVG:"0%"},{drawSVG:"30% 100%"});
-		TweenLite.fromTo(ekg,1,{css:{opacity:"1"}},{css:{opacity:"0",delay:1}});
+		TweenLite.fromTo(ekg,0.7,{css:{opacity:"0.0000001"}},{css:{opacity:"1"}});
+		TweenLite.fromTo(ekg,0.5,{drawSVG:"0%"},{drawSVG:"0% 100%"});
+		TweenLite.fromTo(ekg,0.5,{drawSVG:"100%"},{drawSVG:"98% 100%",delay:1.85});
+		TweenLite.fromTo(ekg,0.7,{css:{opacity:"1"}},{css:{opacity:"0"},delay:1.85});
 
 		setTimeout(ekgPump4,2000);
 	}
@@ -46,9 +53,11 @@
 	function ekgPump4(){
 		console.log("ekgPump4");
 
-		TweenLite.fromTo("#flatline",2,{css:{opacity:"0.0000001"}},{css:{opacity:"1"}});
-		TweenLite.fromTo("#flatline",1,{drawSVG:"0%"},{drawSVG:"30% 100%"});
-		TweenLite.fromTo("#flatline",1,{css:{opacity:"1"}},{css:{opacity:"0",delay:2}});
+		TweenLite.to(ekg,0.1,{css:{opacity:"0"}});
+		TweenLite.fromTo(flatline,0.7,{css:{opacity:"0.0000001"}},{css:{opacity:"1"}});
+		TweenLite.fromTo(flatline,0.5,{drawSVG:"0%"},{drawSVG:"0% 100%"});
+		TweenLite.fromTo(flatline,0.5,{drawSVG:"100%"},{drawSVG:"98% 100%",delay:1.85});
+		TweenLite.fromTo(flatline,0.7,{css:{opacity:"1"}},{css:{opacity:"0"},delay:1.7});
 
 		setTimeout(ekgPump,2000);
 
